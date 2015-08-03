e9a10322ce94ae4b38eed3cc4e9546a1e91d18ea
Michael Bunker & Jennifer Bell <student@epicodus.com>
2015-08-03 13:39:59 -0700

    Add 4th spec

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -1,15 +1,16 @@
 class String
-  define_method(:title_case) do
-    self.capitalize!()
-  end
-end
 
-class String
   define_method(:title_case) do
+    word_exceptions = [ "a", "an", "am", "from", "the", "and", "but", "or", "nor", "for", "yet", "so", "although", "because", "since", "unless", "despite", "of", "in", "to" ]
     split_sentence = self.split
-    split_sentence.each do |word|
+    split_sentence.each_with_index do |word, index|
+
+    if word_exceptions.include?(word) && index != 0
+      word.downcase!()
+    else
       word.capitalize!()
     end
-    split_sentence.join(" ")
   end
+  split_sentence.join(" ")
+end
 end
