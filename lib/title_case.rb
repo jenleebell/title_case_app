54eec84af8260ccf90c324e8e4d8ebb880fe8be3
Ryan Heupler & Jennifer Bell <student@epicodus.com>
2015-08-05 10:56:34 -0700

    Will pass title_case spec in Sinatra

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -1,7 +1,8 @@
 class String
 
   define_method(:title_case) do
-    word_exceptions = [ "a", "an", "am", "from", "the", "and", "but", "or", "nor", "for", "yet", "so", "although", "because", "since", "unless", "despite", "of", "in", "to" ]
+    word_exceptions = [ "a", "an", "am", "from", "the", "and", "but", "or", "nor", "for", "yet", "so", "although", "because", "since",
+      "unless", "despite", "of", "in", "to" ]
     split_sentence = self.split
     split_sentence.each_with_index do |word, index|
       if word_exceptions.include?(word) && index != 0
@@ -12,5 +13,5 @@ class String
     end
     split_sentence.join(" ")
   end
-  
+
 end
